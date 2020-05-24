62c846e2a907f45fe5ab20c2def9abb6a5397c55
Marius Tolzmann <m@rius.berlin>
2020-05-24 02:24:18 +0200

    Finalize inline policy generation

--- main.tf
+++ main.tf
@@ -27,10 +27,40 @@ data "aws_iam_policy_document" "policy" {
     for_each = var.policy_statements
 
     content {
-      actions   = try(statement.value.actions, null)
-      effect    = try(statement.value.effect, null)
-      resources = try(statement.value.resources, null)
-      sid       = try(statement.value.sid, null)
+      sid           = try(statement.value.sid, null)
+      effect        = try(statement.value.effect, null)
+      actions       = try(statement.value.actions, null)
+      not_actions   = try(statement.value.not_actions, null)
+      resources     = try(statement.value.resources, null)
+      not_resources = try(statement.value.not_resources, null)
+
+      dynamic "principals" {
+        for_each = try(statement.value.principals, [])
+
+        content {
+          type        = principals.value.type
+          identifiers = principals.value.identifiers
+        }
+      }
+
+      dynamic "not_principals" {
+        for_each = try(statement.value.not_principals, [])
+
+        content {
+          type        = not_principals.value.type
+          identifiers = not_principals.value.identifiers
+        }
+      }
+
+      dynamic "condition" {
+        for_each = try(statement.value.conditions, [])
+
+        content {
+          test     = condition.value.test
+          variable = condition.value.variable
+          values   = condition.value.values
+        }
+      }
     }
   }
 }
